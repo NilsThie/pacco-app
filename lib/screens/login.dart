7dc1013cfd5563f46f8263daa740ec05308b0daf
NilsThie <32572062+NilsThie@users.noreply.github.com>
2019-07-22 12:32:57 +0200

    start-screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -9,11 +9,23 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppBarTheme(
-         
-      ),
-      
-    );
+    return DecoratedBox(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/Pacco Screens iPhone 6.png'),
+          ),
+          /*
+          gradient: LinearGradient(
+            begin: const Alignment(0.0, -0.1),
+            end: const Alignment(0.0, 0.6),
+            colors:[
+              const Color(0xffef5350),
+              const Color(0x00ef5350)
+            ],*/
+          ),
+              
+      );
+    
   }
 }
+
